72c7131f417f03fcb7b4a87e32065a6ea79437ce
slayqueen2026 <bp08@howardacademy.edu.pa>
2026-07-06 23:57:46 -0500

    Update codigo_de_bbg.ino

--- src/codigo_de_bbg.ino
+++ src/codigo_de_bbg.ino
@@ -1,10 +1,9 @@
 #include <Servo.h>
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <VL53L0X.h>
 
-
-#define I2C_SLAVE_ADDRESS 8 // Slave address 
+#define I2C_SLAVE_ADDRESS 8 // Slave address
 
 struct VisionData {
   float yaw_angle;
@@ -18,42 +17,57 @@ struct VisionData {
 VisionData visionData; // Global instance of the data structure
 
 Servo myservo;
-const int l = 2;
-const int f = 1;
-const int r = 0;
-double Fdist = 300, Ldist = 300, Rdist = 300;
+const int l = 2; //left pov
+const int f = 1; // front pov
+const int r = 0; // right pov
+double Fdist = 300, Ldist = 300, Rdist = 300; // random values
 
-const int ENA = 2; // Digital pins for ENA2(puente H)
-const int IN1 = 3; // Digital pins for IN1(puente H)
-const int IN2 = 4; // Digital pins for IN2(puente H)
+//servo reference
+const int CENTER_ANGLE  = 83;  //91
+const int MIN_ANGLE     = 68;     //-15
+const int MAX_ANGLE     = 98;    //+15
 
 VL53L0X sensors[3]; //Number of distance sensors I have
-const int xshutPins[] = {5, 6, 7}; //Digital pins for each sensor right-f-left
+const int xshutPins[]     = {5, 6, 7}; //Digital pins for each sensor r - f - l
 const uint8_t addresses[] = {0x30, 0x31, 0x32};  // Define unique I2C addresses for each sensor. Make sure these addresses do not conflict with other I2C devices
 
-const int keyPin = 9; //the number of the key pin 9
+const int keyPin = 9; //pin 9 for button
 
-char side = ' ';
-short corners = 0;
-int servo_direction = 0;
-int actual_Dist= 0;
-bool run = false;
+//L298 Control pins
+const int ENA = 10;  // PWM speed
+const int IN1 = 4;   // Direction A
+const int IN2 = 3;   // Direction B
 
-void stop () //stop motors
-{
-  digitalWrite(IN2, LOW);
+const int DEFAULT_SPEED = 255;//180;
+
+char  side        = ' '; //para decir hacia adonde va la vuelta
+short corners     = 0; // variable for coner counting
+int   actual_Dist = 0;
+bool  run         = false;
+
+void stop() {
+  analogWrite(ENA, 0);
   digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
 }
 
-void move (int speed) //Move forward and backward
-{
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN1, LOW);
-  analogWrite(ENA,  abs(speed));
-  if (speed >= 0)
-      digitalWrite(IN1,  HIGH);
-  else
-      digitalWrite(IN2,  HIGH);
+// + = forward, - = backward
+void move(int speed, int duration_ms) {
+  //speed = constrain(speed, -255, 255);
+
+  if (speed > 0) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  } else if (speed < 0) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else {
+    stop();
+    return;
+  }
+
+  analogWrite(ENA, abs(speed));
+  delay(duration_ms);
 }
 
 double dist (int space) // looks at the sensor distance
@@ -61,53 +75,89 @@ double dist (int space) // looks at the sensor distance
   return sensors[space].readRangeContinuousMillimeters();
 }
 
- void turn(char opts)
+ void turn() // turn logic
 {
   corners++;
-  myservo.write(opts=='l'? 130: 50);
-  /*if (opts == 'l')
-     myservo.write(130);
-  else if (opts == 'r') 
-     myservo.write(50);*/
-  //Serial.println(corners);
-  delay(700);
+
+  myservo.write(side=='l'? MIN_ANGLE: MAX_ANGLE);
+  Serial.print("Corner :");
+  Serial.println (corners);
+  move(DEFAULT_SPEED, 600);
+  delay(450);
+  myservo.write(CENTER_ANGLE);
+  while (dist(side == 'l'? l: r) > 500)
+    {
+    delay (50);
+    continue;
+    }
+  Serial.println ("OUT corner ");
 }
 
-void turn()
-{
-  corners++;
-  move(100);
-  myservo.write(servo_direction);
-  delay((corners == 1? 1000: 700)); //check time
-  myservo.write(90);
-  delay(1000);
-  //while (dist(side == 'l'? l : r) > 400);
-  stop();
+int calculateP(double current_distance, char side) {
+    // Tuning values will be different here because they aren't scaled by time
+    const double setpoint = 185.0;
+
+    double error = setpoint - current_distance;
+
+    int target_angle = CENTER_ANGLE;
+    if (side == 'l')
+      target_angle += (int)(error/setpoint * 15);
+    else
+      target_angle -= (int)(error/setpoint * 15);
+    return constrain(target_angle, MIN_ANGLE, MAX_ANGLE);
+    /*// Constrain
+    if (target_angle < MIN_ANGLE) target_angle = MIN_ANGLE;
+    if (target_angle > MAX_ANGLE) target_angle = MAX_ANGLE;
+    return target_angle;*/
 }
 
-void pre_side_def()
+void pre_side_def() // tells you to which side you have to turn
 {
   side =' ';
-  move(100);
   while (side == ' ')
   {
-
+   move(DEFAULT_SPEED, 10);
    Ldist = dist(l);
    Rdist = dist(r);
    Fdist = dist(f);
-   Serial.println("L");
+   Serial.println("LRF");
    Serial.println(Ldist);
    Serial.println(Rdist);
    Serial.println(Fdist);
-   delay(500);
-
       if ((Ldist > 1000) || (Rdist > 1000))
       {
             stop();
             side = (Ldist > 1000? 'l': 'r');
-            servo_direction = (side == 'l'? 140: 40);
+            Serial.println(side);
       }
   }
+}
+
+void free_round() //Stop instead of turn free round
+{
+  corners = 0;
+
+  while (corners < 12)
+  {
+    move(DEFAULT_SPEED, 20);
+
+    actual_Dist = (side == 'l') ? dist(l) : dist(r);
+
+    // Control PID normal
+
+    if (actual_Dist > 1000)
+      {
+        stop();
+        turn();
+      }  
+    else
+      {
+        int servoPos = calculateP(actual_Dist, side);
+        myservo.write(servoPos);
+        //Serial.println(servoPos);
+      }
+  }
+  stop();
 }
 
 void non_free_round()
@@ -116,8 +166,12 @@ void non_free_round()
   while (1)
   {
     if (corners >= 12)
+    { //logic for parallel parking
+
       break;
-    move(50);
+    }
+     
+    move(DEFAULT_SPEED, 20);
     Wire.requestFrom(I2C_SLAVE_ADDRESS, sizeof(VisionData));
 
     // Check if the slave sent the correct number of bytes
@@ -125,7 +179,7 @@ void non_free_round()
       // Read the incoming bytes directly into the struct
       Wire.readBytes((byte*)&visionData, sizeof(VisionData));
       Fdist = visionData.linear_distance;
-      if (Fdist < 600)
+      if (Fdist < 600)  //validate for middle section while turning.
       {
         if (side != ' ')
             turn();
@@ -146,7 +200,7 @@ void non_free_round()
         {
 
         }  
-      
+     
     } else {
       Serial.println("Error: Did not receive expected number of bytes from slave.");
     }
@@ -156,76 +210,39 @@ void non_free_round()
   }
 }
 
-void free_round()
-{
-  corners = 0;
-  //int actual_distance = 0;
-  while(1)
-  {
-    turn();
-    if (corners < 12)
-    {
-      move (100);
-      if (side == 'l')
-      {
-        actual_Dist = dist(l);
-        while (actual_Dist < 1000)
-        {
-          actual_Dist = dist(l);
-          if (actual_Dist < 200)  
-            myservo.write(40);
-          else if (actual_Dist > 300)
-            myservo.write(140);
-          delay (250);
-          myservo.write(90);
-        }
-      }
-      else
-      {
-        actual_Dist = dist(r);
-        while (actual_Dist < 1000)
-        {
-          actual_Dist = dist(r);
-          if (actual_Dist < 200)  
-            myservo.write(140);
-          else if (actual_Dist > 300)
-            myservo.write(40);
-          delay (250);
-          myservo.write(90);
-        }
-      }
-    }
-    else
-    {
-      delay(500);
-      stop();
-      break;
-    }
-  }
-}
 
-void setup() {
+
+
+
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin (9600);
-  pinMode (IN1, OUTPUT);
-  pinMode (IN2, OUTPUT);
-  pinMode (ENA, OUTPUT);
+ 
 
-  myservo.attach(8);//attachs the servo on pin 9 to servo object
-  myservo.write(90);//back to 0 degrees
+  myservo.attach(8);//attachs the servo on pin 8 to servo object
+  myservo.write(CENTER_ANGLE);//back to center
+
   delay(1000);//wait for a second
-  
-  pinMode(keyPin,INPUT);//initialize the key pin as inp
+ 
+  pinMode(keyPin,INPUT);//initialize the key pin as input
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  stop();
 
   Wire.begin();  //Serial.begin(115200);
+
+  short s_count = sizeof(sensors) / sizeof(sensors[0]);
    // Initialize each sensor
-  for (int i = 0; i < sizeof(sensors) / sizeof(sensors[0]); i++) {
+  for (int i = 0; i < s_count; i++) {
     pinMode(xshutPins[i], OUTPUT);
     digitalWrite(xshutPins[i], LOW);  // Keep the sensor in reset state
   }
 
   // Set up each sensor with a unique I2C address
-  for (int i = 0; i < sizeof(sensors) / sizeof(sensors[0]); i++) {
+  for (int i = 0; i < s_count ; i++) {
     digitalWrite(xshutPins[i], HIGH);  // Bring the sensor out of reset
     delay(10);  // Allow time for the sensor to initialize
 
@@ -234,50 +251,53 @@ void setup() {
     sensors[i].setAddress(addresses[i]);
   }
 
-  for (int i = 0; i < sizeof(sensors) / sizeof(sensors[0]); i++) {
+  for (int i = 0; i < s_count; i++) {
   sensors[i].startContinuous();
   }
+  run = true;   //true -> false
 }
 
 
-void loop() {
-  /*
-  myservo.write(90);//back to 0 degrees
-  delay(10000);
-  myservo.write(50);//back to 0 degrees
-  delay(2000);
-  myservo.write(130);//back to 0 degrees
-  delay(2000);*/
+void loop()
+{
   stop();
-
+ 
   while (!run)    // starting button
   {
-   // run = DigitalRead(bpin    
-   
-    if(digitalRead(keyPin) ==HIGH )
+ 
+    if(digitalRead(keyPin) == LOW )  
       {
         stop();
         run = true; //validate the starting button
+        break;
       }
-      Serial.write("\nWaiting button...");
-      stop();
+    else
+      run = false;
+     
+    Serial.write("\nWaiting button...");
+    delay(500);
+  }
+ 
+    while (false)
+    {
+    Ldist = dist(l);
+    Rdist = dist(r);
+    Fdist = dist(f);
+    Serial.println("LRF");
+    Serial.println(Ldist);
+    Serial.println(Rdist);
+    Serial.println(Fdist);
+    delay(1000);
     }
 
-  while(0)
-   {
-   Ldist = dist(l);
-   Rdist = dist(r);
-   Fdist = dist(f);
-   Serial.println("L");
-   Serial.println(Ldist);
-   Serial.println(Rdist);
-   Serial.println(Fdist);
-   delay(500);
-   //break;
-   }
+    pre_side_def();
 
-  pre_side_def();
-  free_round();
-  run = false;
+    Fdist = 400; //dist(f);
+    if (Fdist < 100) {
+      non_free_round();
+    } else {
+      free_round();
+    }
+    run = false;
 
 }
